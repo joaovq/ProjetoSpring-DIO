a02590fa46375a9856b60d9f931b59f9fafcdc0a
João Vítor <vitorsnta90@gmail.com>
2022-07-07 22:11:45 -0300

    update- complement lesson project

--- src/main/java/com/project/ProjetoSpringDIO/service/impl/ClientServiceImpl.java
+++ src/main/java/com/project/ProjetoSpringDIO/service/impl/ClientServiceImpl.java
@@ -1,33 +1,60 @@
 package com.project.ProjetoSpringDIO.service.impl;
 
 import com.project.ProjetoSpringDIO.model.Cliente;
+import com.project.ProjetoSpringDIO.model.ClienteRepository;
+import com.project.ProjetoSpringDIO.model.Endereco;
+import com.project.ProjetoSpringDIO.model.EnderecoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClientServiceImpl implements ClienteService{
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private  ViaCepService viaCepService;
 
     @Override
     public Iterable<Cliente> buscarTodos() {
-        return null;
+       return clienteRepository.findAll();
     }
-
     @Override
     public Cliente buscarPorId(Long id) {
-        return null;
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.get();
     }
 
     @Override
     public void inserir(Cliente cliente) {
-
+        salvarClientePorCep(cliente);
     }
 
     @Override
     public void atualizar(Long id, Cliente cliente) {
+        Optional<Cliente> clienteBancoDeDados = clienteRepository.findById(id);
+        if(clienteBancoDeDados.isPresent()){
+            salvarClientePorCep(cliente);
+        }
 
     }
 
     @Override
     public void deletar(Long id) {
+        clienteRepository.deleteById(id);
+    }
 
+    private void salvarClientePorCep(Cliente cliente) {
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet( () -> {
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
     }
 }
--- src/main/java/com/project/ProjetoSpringDIO/service/impl/ViaCepService.java
+++ src/main/java/com/project/ProjetoSpringDIO/service/impl/ViaCepService.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "viacep", url = "https://viacep;com.br/ws")
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
 
-    // Poderia usar também a anotação @GetMapping, ja indicando que é um Get
+    // Poderia usar também a anotação @GetMapping, ja indicando que é um Get  passanto somente
+    // (value ="/{cep}/json/" ) como parametro
     @RequestMapping(method = RequestMethod.GET,value ="/{cep}/json/" )
     Endereco consultarCep(@PathVariable("cep") String cep);
 }
